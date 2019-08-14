cdf2e8d52e4974a3ecd52f2af474c07aa4a67b8c
Jason Boerner <jboerner56@gmail.com>
2019-08-14 13:10:58 -0400

    basic java class notes

---
+++ intro.java
@@ -0,0 +1,23 @@
+// 8 variable types
+// 7 numeric and boolean
+// 7 numeric
+    // 6 signed
+        // uses numbers
+        // byte, short, int, float, double
+        // positive or negative
+    // 1 unsigned
+        // char
+        // will only take positive numbers
+
+// classes
+// only one class can be a 'public class'
+// what can be used
+    // outside class structure
+        // comments
+        // declare package
+            // can only declare up to one per file
+            // acts as folder
+        // import other files to be used in the class
+
+    // inside class
+        //
