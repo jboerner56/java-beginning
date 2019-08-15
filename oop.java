ef87f1238ddbb52ac4130865415d03fd2d2d928c
Jason Boerner <jboerner56@gmail.com>
2019-08-15 13:08:00 -0400

    basic object structure

---
+++ oop.java
@@ -0,0 +1,21 @@
+
+class Book {
+    // attributes of a class
+    // all objects of a class have the same attributes, (keys) with different data (values)
+    string title;
+    string author;
+    string category;
+}
+
+// create student object
+class createStudents {
+    public static void main(String[] args) {
+        Student one = new Student();
+        one.name = "Jason"
+        one.rollNo = "100";
+
+        Student two = new Student();
+        two.name = "sierra";
+        two.rollNo = "101";
+    }
+}
