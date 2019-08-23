fc534d179bb078e644d90c1c520ea46c7a49674d
Jason Boerner <jboerner56@gmail.com>
2019-08-23 10:25:21 -0400

    beginning expressions

---
+++ JavaExpressions/src/Expressions.java
@@ -0,0 +1,9 @@
+public class Expressions {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
---
+++ JavaExpressions/src/PrimitiveTypes.java
@@ -0,0 +1,2 @@
+public class PrimitiveTypes {
+}
