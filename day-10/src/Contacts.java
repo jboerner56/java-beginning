07b85563fae8385080a25e2b40dc3b304a21edc1
Jason Boerner <jboerner56@gmail.com>
2019-09-05 12:06:15 -0400

    variables and getter/setters

--- day-10/src/Contacts.java
+++ day-10/src/Contacts.java
@@ -1,3 +1,67 @@
 public class Contacts {
-    
+
+    private String firstName;
+    private String lastName;
+//    array of up to 3 mobile numbers
+//    private long mobileNumber[] = new long[3];
+    private long mobilePrimary;
+    private long mobileSecondary;
+    private String countryCode;
+
+    public Contacts() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getMobilePrimary() {
+        return mobilePrimary;
+    }
+
+    public void setMobilePrimary(long mobilePrimary) {
+        this.mobilePrimary = mobilePrimary;
+    }
+
+    public long getMobileSecondary() {
+        return mobileSecondary;
+    }
+
+    public void setMobileSecondary(long mobileSecondary) {
+        this.mobileSecondary = mobileSecondary;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public Contacts(String firstName, String lastName, long mobilePrimary, long mobileSecondary, String countryCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobilePrimary = mobilePrimary;
+        this.mobileSecondary = mobileSecondary;
+        this.countryCode = countryCode;
+
+    }
+    // object creation
+
+    // updating data
+
+
 }
